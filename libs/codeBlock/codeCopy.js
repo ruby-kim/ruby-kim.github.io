20ad344fd4ea0f3f1bbe0eb38f2b6846125fd2b2
ruby-kim <dev.rubykim@gmail.com>
2026-08-18 13:42:22 +0900

    Site updated: 2026-08-18 13:42:21

--- libs/codeBlock/codeCopy.js
+++ libs/codeBlock/codeCopy.js
@@ -1,33 +1,43 @@
-// 클릭 이벤트 수정
-$(".code-area .fa-copy").on("click", function () {
-  const codeElement = $(this).siblings("pre").find("code")[0];
-  const text = codeElement.innerText || codeElement.textContent;
+$(function () {
+  var $copyIcon = $(
+    '<i class="fas fa-copy code_copy" title="copy" aria-hidden="true"></i>'
+  );
+  var $notice = $('<div class="codecopy_notice"></div>');
 
-  navigator.clipboard
-    .writeText(text)
-    .then(() => {
-      $(this)
-        .prev(".codecopy_notice")
-        .text("COPY")
-        .animate({ opacity: 1, top: 30 }, 450, () => {
-          setTimeout(() => {
-            $(this)
-              .prev(".codecopy_notice")
-              .animate({ opacity: 0, top: 0 }, 650);
-          }, 400);
-        });
-    })
-    .catch((err) => {
-      $(this)
-        .prev(".codecopy_notice")
-        .text("复制失败")
-        .animate({ opacity: 1, top: 30 }, 650, () => {
-          setTimeout(() => {
-            $(this)
-              .prev(".codecopy_notice")
-              .animate({ opacity: 0, top: 0 }, 650);
-          }, 400);
-        });
-      console.error("Copy failed", err);
-    });
+  $(".code-area").each(function () {
+    $(this).prepend($copyIcon.clone());
+    $(this).prepend($notice.clone());
+  });
+
+  $(".code-area .fa-copy").on("click", function () {
+    var codeElement = $(this).siblings("pre").find("code")[0];
+    if (!codeElement) return;
+
+    var text = codeElement.innerText || codeElement.textContent;
+    var $icon = $(this);
+
+    navigator.clipboard
+      .writeText(text)
+      .then(function () {
+        $icon
+          .prev(".codecopy_notice")
+          .text("COPY")
+          .animate({ opacity: 1, top: 30 }, 450, function () {
+            setTimeout(function () {
+              $icon.prev(".codecopy_notice").animate({ opacity: 0, top: 0 }, 650);
+            }, 400);
+          });
+      })
+      .catch(function (err) {
+        $icon
+          .prev(".codecopy_notice")
+          .text("COPY FAILED")
+          .animate({ opacity: 1, top: 30 }, 650, function () {
+            setTimeout(function () {
+              $icon.prev(".codecopy_notice").animate({ opacity: 0, top: 0 }, 650);
+            }, 400);
+          });
+        console.error("Copy failed", err);
+      });
+  });
 });
